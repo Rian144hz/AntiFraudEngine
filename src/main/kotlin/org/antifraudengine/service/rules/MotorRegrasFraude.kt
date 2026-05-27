9d314cd00f42a0728d6fd58d90361c8f929bf8bd
rian144hz <rianmatheus669@gmail.com>
2026-05-27 17:26:29 -0300

    feat: implementa MotorRegrasFraude como orquestrador centralizador de regras

---
+++ src/main/kotlin/org/antifraudengine/service/rules/MotorRegrasFraude.kt
@@ -0,0 +1,25 @@
+package org.antifraudengine.service.rules
+
+import org.antifraudengine.domain.entities.Transacao
+import org.antifraudengine.repository.TransacaoRepository
+
+class MotorRegrasFraude(private val regras: List<RegraFraude>,
+    private val transacaoRepository: TransacaoRepository) {
+
+
+    fun processarTransacao(transacao: Transacao) : Transacao {
+
+        for (regra in regras) {
+            val resultado = regra.avaliar(transacao);
+
+            if (!resultado.aprovado) {
+                transacao.status = "NEGADA!"
+                transacao.tipoStatus = resultado.motivo;
+                return transacaoRepository.save(transacao);
+            }
+        }
+        transacao.status = "APROVADA"
+        transacao.tipoStatus = "APROVADA: Nenhuma irregularidade detectada."
+        return transacaoRepository.save(transacao)
+    }
+}
