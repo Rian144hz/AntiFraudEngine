04abeae74f46513b931d97fb7df6a5e4ff355fca
Rian <rian@exemplo.com>
2026-07-18 10:00:53 -0300

    chore: atualização diária automática (2026-07-18 10:00:53 -0300)

--- src/main/kotlin/org/antifraudengine/controller/TransacaoController.kt
+++ src/main/kotlin/org/antifraudengine/controller/TransacaoController.kt
@@ -48,8 +48,7 @@ class TransacaoController(
         var resultadoFiltrado = transacoes;
         if (buyerId != null) {
             resultadoFiltrado = resultadoFiltrado.filter { it.compradorId == buyerId }
-        }
-
+        
         if (status != null) {
             resultadoFiltrado = resultadoFiltrado.filter { it.status.equals(status,ignoreCase = true) }
         }
